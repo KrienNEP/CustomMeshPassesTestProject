daf0fe25b3cbb8a2ff6501d698e644e67fbe3291
Krien Linnenbank <krien.linnenbank@nepgroup.com>
2023-01-11 16:50:22 +0100

    Initial project commit

---
+++ Source/CustomMeshPass/CustomMeshPassGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomMeshPassGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMMESHPASS_API ACustomMeshPassGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
